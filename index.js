d3882f6c42376c19c10fa219479486d64183fc65
Elad Nava <eladnava@gmail.com>
2016-03-27 01:36:28 +0300

    Warn if --chars not specified as CLI argument

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ program
     .option('-c, --chars <$#@>', 'generate icons for custom characters (no seperator)')
     .parse(process.argv);
 
+// Developer didn't provide any custom characters?
+if (!program.chars) {
+    // Let them know it's possible to do so via CLI argument
+    log.warn('material-letter-icons', 'Generating icons for the entire alphabet.');
+    log.warn('material-letter-icons', 'Specify --chars XYZ to override this behavior.');
+}
+else {
+    // Let them know it's possible to do so via CLI argument
+    log.info('material-letter-icons', 'Generating icons for custom characters: ' + program.chars);
+}
+
 // Generate the icons and export them to dist/
 generator.generateIcons(program, function (err) {
     // Log errors
